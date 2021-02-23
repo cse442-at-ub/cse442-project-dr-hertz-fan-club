2c75c4c0e24d7772ad948bab760b7f17091420bc
Bryce Zhong <36309961+EBryceZ@users.noreply.github.com>
2021-02-23 05:37:38 -0500

    Created main page, and connect all pages we have right now #18
    Guide:1. On landing page, choose More on nav bar, and click the link to main page. 2. Click Post to go to listing page. 3. Click Log out to go back to landing page. 4. I hate html, css and js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 // Empty JS for your own code to be here
+function logout(t) {
+    alert("Are you sure?");
+}
+
+function double_check(t) {
+    var result = confirm(t);
+    if (result === true) {
+    }
+}
+
